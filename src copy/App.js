245f795cb518f929c25cf222518f59619bf32245
audidragonz <108033669+audidragonz@users.noreply.github.com>
2022-06-27 18:53:21 -0700

    Update App.js

--- src copy/App.js
+++ src copy/App.js
@@ -1,18 +1,20 @@
-import {Route, Routes} from "react-router-dom";
-import Layout from "./components/layout/Layout";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/Layout"
 import HomePage from "./components/pages/HomePage";
 import EditPage from "./components/pages/EditPage";
-import WebPreviewPage from "./components/pages/WebPreviewPage";
+import AllPages from "./components/pages/AllPages";
 import ContactPage from "./components/pages/ContactPage";
+import WebPreviewPage from "./components/pages/WebPreviewPage";
 
 function App() {
   return (
     <Layout>
       <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/edit" element={<EditPage />} />
-          <Route path="/web-preview" element={<WebPreviewPage />} />
-          <Route path="/contact" element={<ContactPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/edit" element={<EditPage />} />
+        <Route path="/web-preview" element={<WebPreviewPage />} />
+        <Route path="/allpages" element={<AllPages />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </Layout>
   );
