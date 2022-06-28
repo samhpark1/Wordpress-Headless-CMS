21c26a7b3c2534b6d80315ec243cff33060651e1
audidragonz <108033669+audidragonz@users.noreply.github.com>
2022-06-28 14:31:11 -0700

    Update index.js

--- src copy/index.js
+++ src copy/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import  {BrowserRouter} from "react-router-dom";
-import './index.css';
-import App from './App';
+/* File Taken from:
+Title: React Crash Course For Beginners 2021
+Author: Maximilian Schwarzmüller 
+Date: 2021
+Availability: https://github.com/academind/react-complete-guide-code/blob/zz-reactjs-summary/code/19-finished/src/index.js
+*/
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
-
