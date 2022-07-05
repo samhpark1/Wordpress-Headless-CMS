082b8847268176ce4dc4819b8c9a096620a64fed
samhpark1 <97241842+samhpark1@users.noreply.github.com>
2022-07-05 13:34:56 -0400

    **Updated** Added Login Stuff

--- src copy/App.js
+++ src copy/App.js
@@ -1,22 +1,37 @@
-import { Route, Routes } from "react-router-dom";
-import Layout from "./components/layout/Layout"
+import {Route, Routes} from "react-router-dom";
+import {useState} from 'react'
+import Layout from "./components/layout/Layout";
 import HomePage from "./components/pages/HomePage";
 import EditPage from "./components/pages/EditPage";
+import WebPreviewPage from "./components/pages/WebPreviewPage";
 import AllPages from "./components/pages/AllPages";
 import ContactPage from "./components/pages/ContactPage";
-import WebPreviewPage from "./components/pages/WebPreviewPage";
+import LoginModal from "./components/pages/LoginModal";
+import Backdrop from "./components/pages/Backdrop";
+import "./index.css";
 
 function App() {
-  return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/edit" element={<EditPage />} />
-        <Route path="/web-preview" element={<WebPreviewPage />} />
-        <Route path="/allpages" element={<AllPages />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
-    </Layout>
+
+    const [enteredLogin, setEnteredLogin] = useState(false);
+
+    function LoginHandler(){
+        setEnteredLogin(true);
+    }
+
+    return (
+      <div>
+          {!enteredLogin && <LoginModal onFinish={LoginHandler}/>}
+          {!enteredLogin && <Backdrop onFinish={null}/>}
+          <Layout>
+              <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/edit" element={<EditPage />} />
+                  <Route path="/web-preview" element={<WebPreviewPage />} />
+                  <Route path="/allpages" element={<AllPages />} />
+                  <Route path="/contact" element={<ContactPage />} />
+              </Routes>
+          </Layout>
+      </div>
   );
 }
 
